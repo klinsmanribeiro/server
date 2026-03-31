430e8c03cd167556f76a2035c1ad939e8e5dd348
Klinsman Ribeiro <marcio_aguiar_@hotmail.com>
2026-03-30 22:07:37 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { userRoutes } from './routes/user.routes.ts'
 import { videosRoutes } from './routes/videos.routes.ts';
+import { config } from 'dotenv';
 
+config();
 const app = express();
+
+console.log(process.env.SECRET)
 
 app.use(express.json());
 app.use('/user', userRoutes);
